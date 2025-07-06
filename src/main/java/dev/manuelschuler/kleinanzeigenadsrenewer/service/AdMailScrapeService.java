cbbceb57c6963194df6c20307913dd62202c1715
Tutorialwork <tutorialworktv@gmail.com>
2025-07-06 15:00:36 +0200

    Adapted AdMailScrapeService for new mail design

--- src/main/java/dev/manuelschuler/kleinanzeigenadsrenewer/service/AdMailScrapeService.java
+++ src/main/java/dev/manuelschuler/kleinanzeigenadsrenewer/service/AdMailScrapeService.java
@@ -1,30 +1,32 @@
 package dev.manuelschuler.kleinanzeigenadsrenewer.service;
 
 import jakarta.mail.Message;
+import java.util.List;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.util.List;
-
 public class AdMailScrapeService extends ScrapeService {
 
-    private static final String RENEW_BUTTON_CLASS_NAME = ".cta-button-outline";
-    private static final String AD_PAGE_URL = "s-anzeige";
+  private static final String RENEW_BUTTON_QUERY = "a[title='Anzeige verlängern']";
+  private static final String AD_PAGE_URL = "s-anzeige";
 
-    public AdMailScrapeService(Message mail) {
-        super(mail);
-    }
+  public AdMailScrapeService(Message mail) {
+    super(mail);
+  }
 
-    public String getRenewLink() {
-        Document document = this.getDocument();
-        Elements renewButton = document.select(RENEW_BUTTON_CLASS_NAME);
-        return renewButton.attr("href");
-    }
+  public String getRenewLink() {
+    Document document = this.getDocument();
+    Elements renewButton = document.select(RENEW_BUTTON_QUERY);
+    return renewButton.attr("href");
+  }
 
-    public String getAdName() {
-        Document document = this.getDocument();
-        List<Element> matchingElements = document.select("a").stream().filter(element -> element.attr("href").contains(AD_PAGE_URL)).toList();
-        return matchingElements.getFirst().text();
-    }
+  public String getAdName() {
+    Document document = this.getDocument();
+    List<Element> matchingElements =
+        document.select("a").stream()
+            .filter(element -> element.attr("href").contains(AD_PAGE_URL))
+            .toList();
+    return matchingElements.getFirst().text();
+  }
 }
